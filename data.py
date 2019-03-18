d7eec39cffc528c40292bca3cc58496181edfe0c
Iveli de Paula Sousa <iveli.sousa@gmail.com>
2019-03-18 22:29:45 +0100

    Add first query

--- data.py
+++ data.py
@@ -2,8 +2,15 @@ import psycopg2
 
 # 1. What are the most popular three articles of all time?
 
-firstQuestion='What are the most popular three articles of all time?'
-
+questionOne='What are the most popular three articles of all time?'
+queryOne=(
+    select articles.title, count(*) as views
+    from articles inner join log on log.path
+    like concat('%', articles.slug, '%')
+    where log.status like '%200%' 
+    group by "articles.title, log.path 
+    order by views desc limit 3"
+)
 
 # 2. Who are the most popular article authors of all time?
 
