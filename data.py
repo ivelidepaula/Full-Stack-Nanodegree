98c965d31361f5954d502f900b00a5ebd4b678f1
Iveli de Paula Sousa <iveli.sousa@gmail.com>
2019-03-18 21:28:35 +0100

    Initial commit

---
+++ data.py
@@ -0,0 +1,8 @@
+import psycopg2
+
+# What are the most popular three articles of all time?
+
+# Who are the most popular article authors of all time?
+
+# On which days did more than 1% of requests lead to errors?
+
