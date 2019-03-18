e9651522a0e7584a3855b6ef4cd382fdab06d712
Iveli de Paula Sousa <iveli.sousa@gmail.com>
2019-03-18 22:29:25 +0100

    Add first query

--- data.py
+++ data.py
@@ -1,8 +1,11 @@
 import psycopg2
 
-# What are the most popular three articles of all time?
+# 1. What are the most popular three articles of all time?
 
-# Who are the most popular article authors of all time?
+firstQuestion='What are the most popular three articles of all time?'
 
-# On which days did more than 1% of requests lead to errors?
+
+# 2. Who are the most popular article authors of all time?
+
+# 3. On which days did more than 1% of requests lead to errors?
 
