8bdc3eead873a8d0570a4a2e3254b7fb486e2c28
Iveli de Paula Sousa <iveli.sousa@gmail.com>
2019-03-19 10:51:04 +0100

    Add second query

--- data.py
+++ data.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python3
 import psycopg2
 
 # 1. What are the most popular three articles of all time?
@@ -22,6 +23,20 @@ queryOne="""
 
 
 # 2. Who are the most popular article authors of all time?
-
+questionTwo='Who are the most popular article authors of all time?'
+queryTwo="""
+    select 
+        authors.name, 
+        count(*) as views 
+    from 
+    articles 
+    inner join authors on articles.author = authors.id 
+    inner join log on concat('/article/', articles.slug) = log.path 
+    where 
+        log.status like '%200%' 
+    group by 
+        authors.name 
+    order by 
+        views desc"""
 
 # 3. On which days did more than 1% of requests lead to errors?
